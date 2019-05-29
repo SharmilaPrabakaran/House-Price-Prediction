740aa22b7ea09b47de19a851e0cbcf5aa6109fd3
Sharmila Prabakaran <38150422+SharmilaPrabakaran@users.noreply.github.com>
2019-05-29 16:38:45 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
+from flask import Flask, request, render_template
 import numpy as np
-from flask import Flask, request, jsonify
 import pickle
 
 app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 model = pickle.load(open('model.pkl','rb'))
 
-@app.route('/api',methods=['POST'])
+@app.route('/price', methods = ['POST'])
 def predict():
-    data = request.get_json(force=True)
-    prediction = model.predict([[np.array(data['exp'])]])
+    data = int(request.form['sqft'])
+    prediction = model.predict([[np.array(data)]])
     output = prediction[0]
-    return jsonify(output)
+    return render_template('result.html',prediction = output)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.debug = True
+    app.run()
